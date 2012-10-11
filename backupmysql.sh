8dcce1ed04566e758970887679f66d55c976b0ae
Ameir Abdeldayem <royalware@bitbucket.org>
2012-10-11 08:45:02 -0400

    Fixed bug where $OLDDBS wasn't populated properly if FTP wasn't used Split script from configuration portion A tad of refactoring

--- backupmysql.sh
+++ backupmysql.sh
@@ -5,79 +5,15 @@
 # You are free to modify and distribute this code,
 # so long as you keep my name and URL in it.
 
-# your MySQL server's name
-SERVER=`hostname -f`
+#----------------------Start of Script------------------#
 
-# directory to backup to
-BACKDIR=~/backups/mysql
+function die () {
+    echo >&2 "$@"
+    exit 1
+}
 
-# date format that is appended to filename
-DATE=`date +'%m-%d-%Y-%H%M'`
-
-#----------------------MySQL Settings--------------------#
-
-# your MySQL server's location (IP address is best)
-HOST=localhost
-
-# MySQL username
-USER=root
-
-# MySQL password
-PASS=password
-
-# List all of the MySQL databases that you want to backup in here, 
-# each separated by a space
-DBS="db1 db2"
-
-# set to 'y' if you want to backup all your databases. this will override
-# the database selection above.
-DUMPALL=y
-
-
-#----------------------Mail Settings--------------------#
-
-# set to 'y' if you'd like to be emailed the backup (requires mutt)
-MAIL=y
-
-# email addresses to send backups to, separated by a space
-EMAILS="address@yahoo.com address@usa.com"
-
-SUBJECT="MySQL backup on $SERVER ($DATE)"
-
-#----------------------FTP Settings--------------------#
-
-# set "FTP=y" if you want to enable FTP backups
-FTP=y
-
-# FTP server settings; group each remote server using arrays
-# you can have unlimited remote FTP servers
-FTPHOST[0]="ftphost"
-FTPUSER[0]="username"
-FTPPASS[0]="password"
-FTPDIR[0]="backups"
-
-FTPHOST[1]="ftphost"
-FTPUSER[1]="username"
-FTPPASS[1]="password"
-FTPDIR[1]="mybackups/mysql"
-
-FTPHOST[2]="ftphost"
-FTPUSER[2]="username"
-FTPPASS[2]="password"
-FTPDIR[2]="backups"
-
-# directory to backup to; if it doesn't exist, file will be uploaded to 
-# first logged-in directory; the array indices correspond to the FTP info above
-
-#-------------------Deletion Settings-------------------#
-
-# delete old files?
-DELETE=y
-
-# how many days of backups do you want to keep?
-DAYS=30
-
-#----------------------End of Settings------------------#
+CONFIG=${1:-backupmysql.conf}
+[ -f "$CONFIG" ] && . "$CONFIG" || die "Could not load configuration file ${CONFIG}!"
 
 # check of the backup directory exists
 # if not, create it
@@ -94,8 +30,7 @@ if  [ $DUMPALL = "y" ]; then
 fi
 
 echo "Backing up MySQL databases..."
-for database in $DBS
-do
+for database in $DBS; do
 	echo -n "Backing up database $database..."
 	mysqldump -h $HOST --user=$USER --password=$PASS $database > \
 		$BACKDIR/$SERVER-$database-$DATE-mysqlbackup.sql
@@ -120,40 +55,40 @@ if  [ $MAIL = "y" ]; then
 	fi
 fi
 
-if  [ $FTP = "y" ]; then
-	echo "Initiating FTP connection..."
-	if  [ $DELETE = "y" ]; then
-		OLDDBS=`cd $BACKDIR; find . -name "*-mysqlbackup.sql.gz" -mtime +$DAYS`
-		REMOVE=`for file in $OLDDBS; do echo -n -e "delete ${file}\n"; done`
-	fi
-
-	cd $BACKDIR
-	ATTACH=`for file in *$DATE-mysqlbackup.sql.gz; do echo -n -e "put ${file}\n"; done`
-
-for KEY in "${!FTPHOST[@]}"
-do
-	echo -e "\nConnecting to ${FTPHOST[$KEY]} with user ${FTPUSER[$KEY]}..."
-	ftp -nvp <<EOF
-	open ${FTPHOST[$KEY]}
-	user ${FTPUSER[$KEY]} ${FTPPASS[$KEY]}
-	tick
-	cd ${FTPDIR[$KEY]}
-	$REMOVE
-	$ATTACH
-	quit
-EOF
-done
-
-	echo -e  "FTP transfer complete! \n"
-fi
-
 if  [ $DELETE = "y" ]; then
-	cd $BACKDIR; for file in $OLDDBS; do rm ${file}; done
+	OLDDBS=`cd $BACKDIR; find . -name "*-mysqlbackup.sql.gz" -mtime +$DAYS`
+	REMOVE=`for file in $OLDDBS; do echo -n -e "delete ${file}\n"; done` # will be used in FTP
+		
+	cd $BACKDIR; for file in $OLDDBS; do rm -v ${file}; done
 	if  [ $DAYS = "1" ]; then
 		echo "Yesterday's backup has been deleted."
 	else
 		echo "The backups from $DAYS days ago and earlier have been deleted."
 	fi
 fi
+	
+if  [ $FTP = "y" ]; then
+	echo "Initiating FTP connection..."
+
+	cd $BACKDIR
+	ATTACH=`for file in *$DATE-mysqlbackup.sql.gz; do echo -n -e "put ${file}\n"; done`
+
+	for KEY in "${!FTPHOST[@]}"; do
+		echo -e "\nConnecting to ${FTPHOST[$KEY]} with user ${FTPUSER[$KEY]}..."
+		ftp -nvp <<EOF
+	open ${FTPHOST[$KEY]}
+	user ${FTPUSER[$KEY]} ${FTPPASS[$KEY]}
+	tick
+	mkdir ${FTPDIR[$KEY]}
+	cd ${FTPDIR[$KEY]}
+	$REMOVE
+	$ATTACH
+	quit
+EOF
+		done
+
+	echo -e  "FTP transfer complete! \n"
+fi
 
 echo "Your backup is complete!"
+
